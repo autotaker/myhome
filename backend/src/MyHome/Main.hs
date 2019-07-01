0525800999e445e2c8984c11dedb4297991a96b1
autotaker <autotaker@gmail.com>
2019-07-01 23:39:29 +0900

    Implement DB Connection

--- backend/src/MyHome/Main.hs
+++ backend/src/MyHome/Main.hs
@@ -1,17 +1,43 @@
 {-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module MyHome.Main where
 
-import Web.Scotty
+import qualified Web.Scotty as Scotty
 import Data.Aeson hiding (json)
+import Data.Pool
 import GHC.Generics
+import Control.Monad.IO.Class
+import Database.Persist.MySQL
+import Database.Persist
+import Database.Persist.TH
+import Control.Monad.Logger
+import Data.Text.Lazy(Text)
 
-data Hello = 
-    Hello { message :: String }
-    deriving(Generic)
 
-instance ToJSON Hello
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+Hello json
+    message String
+|]
+
+scottyMain :: Pool SqlBackend -> Scotty.ScottyM ()
+scottyMain backendPool = do
+   Scotty.get "/" $ do
+       Scotty.json $ Hello "Hello World!"
+   Scotty.get "/dbtest" $ do
+       msg <- liftIO $ flip runSqlPool backendPool $ do
+           map entityVal <$> selectList [] []
+       Scotty.json $ (msg :: [Hello])
 
 main :: IO ()
-main = scotty 3000 $ do
-       get "/" $ do
-           json $ Hello { message = "Hello World!" }
+main = do
+    let info = mkMySQLConnectInfo "127.0.0.1" "myhome" "myhome" "myhome_test"
+    pool <- runStdoutLoggingT $ createMySQLPool info 20
+    runStdoutLoggingT $ withMySQLConn info (runSqlConn (runMigration migrateAll))
+    Scotty.scotty 3000 $ scottyMain pool
+       
